d9dea7c386c9c13d30036593858ef65e87d48f87
Kirk Roerig <mr.possoms@gmail.com>
2017-08-25 10:51:21 -0400

    Missed encoder cog code

---
+++ rot-enc-cog.s
@@ -0,0 +1,50 @@
+'
+' This file is part of pwm-logger
+'
+' pwm-logger is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'
+
+' --------------------
+' Rotary encoder Watcher routine
+'
+	ORG 0
+ROTARY_WATCHER
+	MOV      ROTOR_STATE, INA
+	AND      ROTOR_STATE, ROTOR_MSK
+
+:ROTOR_LOOP
+	WAITPNE  ROTOR_STATE, ROTOR_MSK
+	MOV      ROTOR_STATE, INA
+	AND      ROTOR_STATE, ROTOR_MSK
+	
+	ADD      ROTOR_COUNT, #1
+
+	WRLONG   ROTOR_COUNT_PTR, ROTOR_COUNT
+	JMP      #:ROTOR_LOOP
+
+
+
+'	
+' Registers
+'
+ROTOR_COUNT_PTR
+	LONG 0
+
+ROTOR_STATE LONG 0
+ROTOR_COUNT LONG 0
+ROTOR_MSK   LONG $00000000  ' TODO: figure out what this should be
+ROTOR_TMP   LONG 0
+
+'---------------------
+	FIT 496
